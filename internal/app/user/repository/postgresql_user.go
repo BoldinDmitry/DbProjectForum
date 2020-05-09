96ab8375b5f663b5f178515796f63f591e0e160b
dmitry <boldin.dmitry@gmail.com>
2020-05-09 21:43:15 +0300

    print deleted

--- internal/app/user/repository/postgresql_user.go
+++ internal/app/user/repository/postgresql_user.go
@@ -78,7 +78,6 @@ func (p *postgresUserRepository) GetUsersByForum(slug string, limit int, since s
         ORDER BY lower(users.Nickname) LIMIT NULLIF($2, 0)`, since)
 	}
 	var data []models.User
-	fmt.Println(query)
 	err := p.Conn.Select(&data, query, slug, limit)
 	return data, err
 }
