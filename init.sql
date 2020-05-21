83d7b98ab0d7bd5dd68526fcf72696ae4894ff3b
dmitry <boldin.dmitry@gmail.com>
2020-05-21 17:24:11 +0300

    some settings added

--- init.sql
+++ init.sql
@@ -1,3 +1,19 @@
+ALTER SYSTEM SET max_connections = '150';
+ALTER SYSTEM SET shared_buffers = '1GB';
+ALTER SYSTEM SET effective_cache_size = '3GB';
+ALTER SYSTEM SET maintenance_work_mem = '256MB';
+ALTER SYSTEM SET checkpoint_completion_target = '0.7';
+ALTER SYSTEM SET wal_buffers = '16MB';
+ALTER SYSTEM SET default_statistics_target = '100';
+ALTER SYSTEM SET random_page_cost = '1.1';
+ALTER SYSTEM SET effective_io_concurrency = '200';
+ALTER SYSTEM SET work_mem = '6990kB';
+ALTER SYSTEM SET min_wal_size = '1GB';
+ALTER SYSTEM SET max_wal_size = '2GB';
+ALTER SYSTEM SET max_worker_processes = '2';
+ALTER SYSTEM SET max_parallel_workers_per_gather = '1';
+ALTER SYSTEM SET max_parallel_workers = '2';
+
 CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE "users"
