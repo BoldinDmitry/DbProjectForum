9567c1ff25dd25326bf04449d6c73632d03ea2c5
dmitry <boldin.dmitry@gmail.com>
2020-05-21 15:50:49 +0300

    old init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,3 @@
-SET random_page_cost TO 1; --for SSD
-SET default_statistics_target TO 300;
-
 CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE "users"
@@ -163,32 +160,22 @@ CREATE TRIGGER edit_vote
     FOR EACH ROW
 EXECUTE PROCEDURE update_votes();
 
-CREATE INDEX post_first_parent_index ON post ((post.path[1]));
-CREATE INDEX post_thread_index ON post (thread); -- -
-CREATE INDEX post_thread_id_index ON post (id, thread); -- +
+CREATE INDEX post_first_parent_thread_index ON post ((post.path[1]), thread);
+CREATE INDEX post_first_parent_id_index ON post ((post.path[1]), id);
+CREATE INDEX post_path_index ON post ((post.path));
+CREATE INDEX post_thread_index ON post (thread);
 
 CREATE INDEX forum_slug_lower_index ON forum (lower(forum.Slug)); -- +
 
-CREATE INDEX users_nickname_lower_index ON users (lower(users.Nickname));
-CREATE INDEX users_nickname_index ON users ((users.Nickname));
+CREATE INDEX users_nickname_lower_index ON users (lower(users.Nickname)); -- +
+CREATE INDEX users_nickname_email_index ON users (lower(users.Nickname), Email); -- +
 
+CREATE INDEX users_forum_forum_user_index ON users_forum (lower(users_forum.Slug), nickname);
 CREATE INDEX users_forum_user_index ON users_forum (nickname);
 
-CREATE INDEX thread_slug_id_index ON thread (lower(slug), id);
+CREATE INDEX thread_slug_lower_index ON thread (lower(slug));
 CREATE INDEX thread_forum_lower_index ON thread (lower(forum)); -- +
 CREATE INDEX thread_id_forum_index ON thread (id, forum);
 CREATE INDEX thread_created_index ON thread (created);
 
 CREATE INDEX vote_nickname ON vote (lower(nickname), idThread, voice); -- +
-
--- NEW INDEXES
-CREATE INDEX post_path_id_index ON post (id, (post.path));
-
-CREATE INDEX users_forum_forum_index ON users_forum ((users_forum.Slug)); -- +
-
--- SOME MORE INDEXES
-CREATE INDEX thread_slug_id_created_index ON thread (lower(slug), created, id);
-
-CREATE INDEX users_forum_slug_nickname_index ON users_forum (slug, nickname);
-
-CREATE INDEX post_first_parent_thread_index ON post (id, thread);
