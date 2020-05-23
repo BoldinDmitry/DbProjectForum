dccf58677c29fabc319836ccb4f8c31bef4e61ab
dmitry <boldin.dmitry@gmail.com>
2020-05-23 18:50:36 +0300

    new config

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 ALTER SYSTEM SET
-    max_connections = '200';
+    max_connections = '300';
 ALTER SYSTEM SET
-    shared_buffers = '200MB';
+    shared_buffers = '200GB';
 ALTER SYSTEM SET
-    effective_cache_size = '600MB';
+    effective_cache_size = '600GB';
 ALTER SYSTEM SET
-    maintenance_work_mem = '50MB';
+    maintenance_work_mem = '2GB';
 ALTER SYSTEM SET
-    checkpoint_completion_target = '0.7';
+    checkpoint_completion_target = '0.9';
 ALTER SYSTEM SET
-    wal_buffers = '6MB';
+    wal_buffers = '16MB';
 ALTER SYSTEM SET
     default_statistics_target = '100';
 ALTER SYSTEM SET
@@ -17,17 +17,19 @@ ALTER SYSTEM SET
 ALTER SYSTEM SET
     effective_io_concurrency = '200';
 ALTER SYSTEM SET
-    work_mem = '1MB';
+    work_mem = '699050kB';
 ALTER SYSTEM SET
-    min_wal_size = '1GB';
+    min_wal_size = '2GB';
 ALTER SYSTEM SET
-    max_wal_size = '4GB';
+    max_wal_size = '8GB';
 ALTER SYSTEM SET
     max_worker_processes = '2';
 ALTER SYSTEM SET
     max_parallel_workers_per_gather = '1';
 ALTER SYSTEM SET
     max_parallel_workers = '2';
+ALTER SYSTEM SET
+    max_parallel_maintenance_workers = '1';
 
 CREATE EXTENSION IF NOT EXISTS citext;
 
