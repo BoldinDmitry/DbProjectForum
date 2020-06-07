86c02a0d4aa601842ffc1426713fe63b66043901
dmitry <boldin.dmitry@gmail.com>
2020-06-07 19:41:04 +0300

    config changed

--- init.sql
+++ init.sql
@@ -1,5 +1,22 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
+ALTER SYSTEM SET
+    checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+    wal_buffers = '6912kB';
+ALTER SYSTEM SET
+    default_statistics_target = '100';
+ALTER SYSTEM SET
+    random_page_cost = '1.1';
+ALTER SYSTEM SET
+    effective_io_concurrency = '200';
+ALTER SYSTEM SET
+    max_worker_processes = '2';
+ALTER SYSTEM SET
+    max_parallel_workers_per_gather = '1';
+ALTER SYSTEM SET
+    max_parallel_workers = '2';
+
 CREATE UNLOGGED TABLE "users"
 (
     About    text,
